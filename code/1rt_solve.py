f825c209834571f7979439a37e48e66142ec83a3
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2023-03-18 16:43:57 -0400

    Create 1rt_solve.py

---
+++ code/1rt_solve.py
@@ -0,0 +1,47 @@
+def is_subsequence(l: list, subl: list):
+    pos=0
+    for i in l:
+        if i == subl[pos]:
+            pos+=1
+        if pos == len(subl):
+            return True
+    return False
+
+def is_valid(l: list):
+    n= 0
+    for i in l:
+        if n < 0:
+            return False
+        if i == '(':
+            n+=1
+        if i == ')':
+            n-=1
+    return n == 0
+
+
+def solve(s:list, t:list):
+    sol= []
+    l= ['']
+    while l:
+        temp= l.pop()
+        if is_subsequence(temp, s) and is_subsequence(temp, t) and is_valid(temp):
+            sol.append(temp)
+        
+        if len(temp) <= 2*len(s+t) -1:
+            l.append(temp + '(')
+            l.append(temp + ')')
+    
+    s= sol[0]
+    solution= []
+    for i in sol:
+        if len(s) > len(i):
+            s= i
+            solution= []
+        if len(s) == len(i):
+            solution.append(i)
+    print(solution)
+    
+
+
+
+solve('()', '(((')
