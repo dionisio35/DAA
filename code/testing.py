904e4126255ce7dc2836f2f987cc3308ebca7afc
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2023-03-29 23:15:01 -0400

    Update testing.py

--- code/testing.py
+++ code/testing.py
@@ -1,20 +1,37 @@
 from generator import generate_n
-from first_solve import solve
+from first_solve import solve as solve1
+from second_solve import solve as solve2
+
+from time import time
+
+def same_solution(s1, s2):
+    for i in s1:
+        if i not in s2:
+            return False
+    return True
 
 
-def test(method:function, times:int, min_len:int,max_len:int):
-    gen = generate_n(times,min_len,max_len)
+def test(method, times:int, min_len:int, max_len:int):
+    gen = generate_n(times, min_len, max_len)
+    
+    print('generated  ', gen)
     for i in range(times):
-        real_solves:list = solve(gen[i][0],gen[i][1])
-        test_solve=method(gen[i][0],gen[i][1])
-        if(real_solves.__contains__(test_solve)):
+        
+        t= time()
+        real_solves:list = solve1(gen[i][0], gen[i][1])
+        print('brute force time', time()- t)
+        t= time()
+        test_solve= method(gen[i][0], gen[i][1])
+        print('test time', time()- t)
+
+        if same_solution(test_solve, real_solves):
             print(f"Solution = {test_solve} that is CORRECT")
         else:
             print(f"Solution = {test_solve} that is INCORRECT")
 
 
 
-
+test(solve2, 1, 10,11)
         
 
 
