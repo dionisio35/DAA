e3e91bc632f9d84ec01a8d6a8f54b05f77017d1a
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2023-04-01 22:46:54 -0400

    small changes

--- code/generator.py
+++ code/generator.py
@@ -1,29 +1,23 @@
 from random import randint
 
-
-
 def generate_n(n:int, min:int, max:int, a="e", b="r"):
     cases=[]
 
     for _ in range(n):
-        len1 = randint(min,max)
-        len2 = randint(min,max)
+        len1 = randint(min, max)
+        len2 = randint(min, max)
 
-        cases.append((generate_string(len1,a,b),generate_string(len2,a,b)))
+        cases.append((generate_string(len1, a, b), generate_string(len2, a, b)))
     return cases
 
 def generate_string(len:int, a="e", b="r"):
     word= ""
 
     for _ in range(len):
-        rnd=randint(0,1)
+        rnd=randint(0, 1)
         
         if(rnd):
             word+=a
         else:
             word+=b
     return word
-            
-# print(generate_n(5,5,10,"e","r"))
-
-
