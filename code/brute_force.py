ea84bbccc71c35929ebf575de41a0a0f7bcb4a2c
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2023-04-01 21:46:14 -0400

    Update brute_force.py

--- code/brute_force.py
+++ code/brute_force.py
@@ -7,18 +7,18 @@ def generate_all_balanced_len_n(n,mem):
     """generate all the sequences balanced of lenth n"""
     n_list=[]
     for c in mem[n-1]:
-        n_list.append(c+"(")
-        n_list.append(c+")")
+        n_list.append(c+"e")
+        n_list.append(c+"r")
     return n_list
 
-def solved(elems,s,t):
+def solved(elems, s, t):
     solv=[]
     for i in elems:
         if(is_subsequence(i, s) and is_subsequence(i, t) and is_valid(i)):
             solv.append(i)
     return solv
 
-def brute_force_algorithm(s:str,t:str):
+def brute_force_algorithm(s:str, t:str):
     """generate all solutions"""
     mem=[[""]]
     for i in range(1,2*(len(s)+len(t))):
@@ -26,14 +26,4 @@ def brute_force_algorithm(s:str,t:str):
         solutions_n=solved(mem[i],s,t)
         if(len(solutions_n)):
             return solutions_n
-
-
-
-print("Solve",solve('()', '((('))
-print("brute_force_algorithm",brute_force_algorithm('()', '((('))
-
-
-
-
-
 
