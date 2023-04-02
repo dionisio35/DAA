6d058f8f7e579506501a8fec5d04af2539790d73
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-04-02 00:50:37 +0000

    Update dp.py

--- code/dp.py
+++ code/dp.py
@@ -30,9 +30,10 @@ def min_sequence(possibles_sequences,a,b):
     m=10e31
     s=""
     for i in possibles_sequences:
-        temp=len(i)+get_n_to_balanced(i,a,b)
-        if temp < m:
-            m=temp
+        i=get_balance(i,a,b)
+        # temp=len(i)+get_n_to_balanced(i,a,b)
+        if len(i) < m:
+            m=len(i)
             s=i
     return s
     
@@ -41,6 +42,26 @@ def min_sequence(possibles_sequences,a,b):
     # if((len(c)+get_n_to_balanced(c,a,b))>(len(v)+get_n_to_balanced(v,a,b))):
     #     return v
     # return c
+
+# def get_better_chain(ct,cs,t,s,a,b):
+#     possibles_sequences=[]
+#     if is_subsequence(ct,s) and is_subsequence(cs,t):
+#         possibles_sequences.append(min_sequence([cs,ct],a,b))
+#     elif is_subsequence(cs,t):
+#         possibles_sequences.append(cs)
+#     elif is_subsequence(ct,s):
+#         possibles_sequences.append(ct)
+
+#     # if is_subsequence(cs,ct) or is_subsequence(cs,t):
+#     #     return cs
+#     # if is_subsequence(ct,cs) or is_subsequence(ct,s):
+#     #     return ct
+
+#     possibles_sequences.append(cs+t[len(t)-1])
+#     possibles_sequences.append(ct + s[len(s)-1])
+#     return min_sequence(possibles_sequences,a,b) 
+
+
 
 def get_better_chain(ct,cs,t,s,a,b):
     possibles_sequences=[]
@@ -59,6 +80,7 @@ def get_better_chain(ct,cs,t,s,a,b):
     possibles_sequences.append(cs+t[len(t)-1])
     possibles_sequences.append(ct + s[len(s)-1])
     return min_sequence(possibles_sequences,a,b) 
+
 
 
 def get_balance(l,a,b):
@@ -85,23 +107,23 @@ def spies_dp(s,t,a="(",b=")"):
     dp=create_matrix(s,t,n,m)
     for i in range(1,len(dp)):
         for j in range(1,len(dp[0])):
-            if(i==3 and j==4):
-                print("here")
+            # if(i==3 and j==4):
+                # print("here")
             dp[i][j] = get_better_chain(dp[i-1][j], dp[i][j-1],dp[0][j],dp[i][0],a,b)
     # return dp[len(dp)-1][ len(dp[0])-1]      
-    print(np.array(dp))      
+    # print(np.array(dp))      
     return get_balance(dp[len(dp)-1][ len(dp[0])-1],a,b)
             
 
 # s="(()("
 # t= ")))"
 
-s=")((()(("      
-t="))(((((()"
+# s=")((()(("      
+# t="))(((((()"
 
-s="()((" 
-t="())("
-a=spies_dp(s,t)
-print(a,len(a))
+# s="()((" 
+# t="())("
+# a=spies_dp(s,t)
+# print(a,len(a))
 
 # print(")"*5)
