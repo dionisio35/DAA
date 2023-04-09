4c8b1e2daa21507e0e9e46c806d338dd88e1ef81
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-04-09 13:16:27 +0000

    new_sols

---
+++ code/dp3a.py
@@ -0,0 +1,88 @@
+from tools import *
+from dp import *
+
+
+def create_matrix(s,t,n,m):
+    dp= [[[] for j in range(m+1)] for i in range(n+1) ] #create the nXm matrix
+    dp[0][0]=[("",0)]
+    return dp
+
+def get_min(l):
+    minm=2e31
+    chain=""
+    for c in l:
+        temp=c[0]+")"*c[1]
+        if len(temp)<minm:
+            minm=len(temp)
+            chain=temp
+    return chain
+
+def is_subsequence(l: list, subl: list):
+    if len(subl)==0:
+        return False
+    pos=0
+    for i in l:
+        if i == subl[pos]:
+            pos+=1
+        if pos == len(subl):
+            return True
+    return False
+
+
+
+
+def dp3(s,t,a="(",b=")"):
+    n=len(s)
+    m=len(t)
+    dp=create_matrix(s,t,n,m)
+    for i in range(len(dp)):
+        for j in range(len(dp[0])):
+            for chain in dp[i][j]:
+                if i+1<=n and j+1<=m:
+                    
+                    if is_subsequence(chain[0],s[0:i-1]) and is_subsequence(chain[0],t[0:j-1]):
+                        dp[i+1][j+1].append((chain[0],chain[1]))
+
+                    if s[i-1] == t[i-1]:
+                        if s[i-1]==")":
+                            if chain[1] > 0:
+                                dp[i+1][j+1].append((chain[0]+")",chain[1]-1))
+                            else:
+                                dp[i+1][j+1].append((chain[0]+"()",0))
+                        else:
+                            dp[i+1][j+1].append((chain[0]+"(",chain[1]+1))
+                    else:
+                        dp[i+1][j+1].append((chain[0]+"()",chain[1]))
+                        if chain[1]>0:
+                            dp[i+1][j+1].append((chain[0]+")(",chain[1]))
+
+                if i+1<=n:
+                    if is_subsequence(chain[0],s[0:i-1]) :
+                        dp[i+1][j].append((chain[0],chain[1]))
+
+                    if s[i-1]==")":
+                        if chain[1]>0:
+                            dp[i+1][j].append((chain[0]+")",chain[1]-1))
+                        else:
+                            dp[i+1][j].append((chain[0]+"()",0))
+                    else:
+                        dp[i+1][j].append((chain[0]+"(",chain[1]+1))
+                
+                if j+1<=m:
+                    if is_subsequence(chain[0],t[0:j-1]):
+                        dp[i][j+1].append((chain[0],chain[1]))
+                    if t[j-1]==")":
+                        if chain[1]>0:
+                            dp[i][j+1].append((chain[0]+")",chain[1]-1))
+                        else:
+                            dp[i][j+1].append((chain[0]+"()",0))
+                    else:
+                        dp[i][j+1].append((chain[0]+"(",chain[1]+1))
+
+    return get_min(dp[n][m])
+    
+s="())("
+t=")((("
+
+a=dp3(s,t)
+print(a)
--- code/dp5a.py
+++ code/dp5a.py
@@ -14,14 +14,12 @@ def dp5(s,t,a="(",b=")"):
     m=len(t)
     dp=create_matrix(s,t,n,m)
     for i in range(len(dp)):
-        for j in range(len(dp[0])):
-            
+        for j in range(len(dp[0])):            
             for k in dp[i][j].keys():
-
                 #i+1,j+1,k
                 if i+1<=n and j+1<=m:
-                    if s[i-1] == t[i-1]:
-                        if s[i-1]==")": 
+                    if s[i] == t[i]:
+                        if s[i]==")": 
                             if k > 0:
                                 dp[i+1][j+1][k-1] = min(dp[i][j][k]+1 ,dp[i+1][j+1][k-1] if dp[i+1][j+1].get(k-1) else 2e31)
                             else:
@@ -30,20 +28,18 @@ def dp5(s,t,a="(",b=")"):
                             dp[i+1][j+1][k+1] = min(dp[i][j][k]+1, dp[i+1][j+1][k+1] if dp[i+1][j+1].get(k+1) else 2e31)
                     else:
                         dp[i+1][j+1][k] = min(dp[i][j][k]+2, dp[i+1][j+1][k] if dp[i+1][j+1].get(k) else 2e31)
-
                 #i+1,j,k
                 if i+1<=n:
-                    if s[i-1]==")":
+                    if s[i]==")":
                         if k>0:
                             dp[i+1][j][k-1] = min(dp[i][j][k]+1, dp[i+1][j][k-1] if dp[i+1][j].get(k-1) else 2e31)
                         else:
                             dp[i+1][j][k] = min(dp[i][j][k]+2, dp[i+1][j][k] if dp[i+1][j].get(k) else 2e31)
                     else:
                         dp[i+1][j][k+1] = min(dp[i][j][k]+1, dp[i+1][j][k+1] if dp[i+1][j].get(k+1) else 2e31)
-
                 #i,j+1,k
                 if j+1<=m:
-                    if t[j-1]==")":
+                    if t[j]==")":
                         if k > 0:
                             dp[i][j+1][k-1] = min(dp[i][j][k]+1, dp[i][j+1][k-1] if dp[i][j+1].get(k-1) else 2e31)
                         else:
